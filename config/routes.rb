b3d596375eb180371547d87738b3bcc14bb75b84
toMilanesi <tomi.milanesi@gmail.com>
2023-02-14 23:22:53 -0300

    Add routes for alumnos, cursos, families and inscriptions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :alumnos
-  resources :familis
-  resources :cursos
+  namespace :api do
+    namespace :v1 do
+      resources :alumnos, only: [:index, :show, :create, :destroy]
+      resources :cursos, only: [:index, :show]
+      resources :families, only: [:index, :show]
+      resources :inscriptions, only: [:index, :show]
+    end
+  end
+  # resources :alumnos
+  # resources :familis
+  # resources :cursos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
