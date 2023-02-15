5e547709d38a7c25d8b978356e899e49183c2923
toMilanesi <tomi.milanesi@gmail.com>
2023-02-14 23:22:05 -0300

    Modify curso model, add dependant delete_all

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,4 +1,4 @@
 class Curso < ApplicationRecord
-  has_many :inscriptions
+  has_many :inscriptions, dependent: :delete_all
   has_many :alumnos, through: :inscription, foreign_key: 'alumno_id'
 end
