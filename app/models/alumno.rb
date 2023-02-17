78385730e40439953c41bea2b42b1737db46d7ef
toMilanesi <tomi.milanesi@gmail.com>
2023-02-17 20:11:39 -0300

    Add assosiations to payments, years and concepts

--- app/models/alumno.rb
+++ app/models/alumno.rb
@@ -2,4 +2,7 @@ class Alumno < ApplicationRecord
   belongs_to :family
   has_one :inscription, dependent: :delete
   has_one :curso, through: :inscription
+  has_many :payments, dependent: :delete_all
+  has_many :years, dependent: :delete_all
+  has_many :concepts, through: :years, dependent: :delete_all
 end
