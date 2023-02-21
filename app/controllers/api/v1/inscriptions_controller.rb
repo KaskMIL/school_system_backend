6927612d839091ee4b8c60fb2e5154302019f04d
toMilanesi <tomi.milanesi@gmail.com>
2023-02-21 10:11:36 -0300

    Add create and destroy controllers, add inscription_params

--- app/controllers/api/v1/inscriptions_controller.rb
+++ app/controllers/api/v1/inscriptions_controller.rb
@@ -10,4 +10,29 @@ class Api::V1::InscriptionsController < ApplicationController
 
     render json: @inscription
   end
+
+  def create
+    @inscription = Inscription.new(inscription_params)
+
+    if @inscription.save
+      render json: { msg: 'Inscripcion creado con exito!', inscription: @inscription }
+    else
+      render json: { error: 'No se ha podido crear la inscripcion, revise los parametros!' }
+    end
+  end
+
+  def destroy
+    @inscription = Inscription.find(params[:id])
+    if @inscription.destroy
+      render json: { msg: 'La inscripcion se ha eliminado con exito!' }
+    else
+      render json: { error: 'La inscripcion no se ha eliminado de la base de datos.' }
+    end
+  end
+
+  private
+
+  def inscription_params
+    params.require(:inscription).permit(:curso, :alumno)
+  end
 end
