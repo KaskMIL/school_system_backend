e96e3497f76cdafba08bf084e622c23bc0780150
toMilanesi <tomi.milanesi@gmail.com>
2023-02-21 10:11:10 -0300

    Add create and destroy controllers, add family_params

--- app/controllers/api/v1/families_controller.rb
+++ app/controllers/api/v1/families_controller.rb
@@ -10,4 +10,29 @@ class Api::V1::FamiliesController < ApplicationController
 
     render json: @family
   end
+
+  def create
+    @family = Family.new(family_params)
+
+    if @family.save
+      render json: { msg: 'Familia creada con exito!', familia: @family }
+    else
+      render json: { error: 'No se ha podido crear la familia, revise los parametros!' }
+    end
+  end
+
+  def destroy
+    @family = Family.find(params[:id])
+    if @family.destroy
+      render json: { msg: 'La familia se ha eliminado con exito!' }
+    else
+      render json: { error: 'La familia no se ha eliminado de la base de datos.' }
+    end
+  end
+
+  private
+
+  def family_params
+    params.require(:familia).permit(:apellido)
+  end
 end
