a45e7dc94e9c3fe3ea5a566ee7e4fdea9c5c140d
toMilanesi <tomi.milanesi@gmail.com>
2023-02-21 10:10:35 -0300

    Add create and destroy controllers, add curso_params

--- app/controllers/api/v1/cursos_controller.rb
+++ app/controllers/api/v1/cursos_controller.rb
@@ -13,4 +13,29 @@ class Api::V1::CursosController < ApplicationController
 
     render json: @alumnos
   end
+
+  def create
+    @curso = Curso.new(curso_params)
+
+    if @curso.save
+      render json: { msg: 'Curso creado con exito!', curso: @curso }
+    else
+      render json: { error: 'No se ha podido crear el curso, revise los parametros!' }
+    end
+  end
+
+  def destroy
+    @curso = Curso.find(params[:id])
+    if @curso.destroy
+      render json: { msg: 'El curso se ha eliminado con exito!' }
+    else
+      render json: { error: 'El curso no se ha eliminado de la base de datos.' }
+    end
+  end
+
+  private
+
+  def curso_params
+    params.require(:curso).permit(:año, :seccion, :nivel, :cantidad)
+  end
 end
